90503cb24035eb02ea4b932ecd548e0861384622
Arthur de Fluiter <arghhhhhthur@gmail.com>
2017-03-29 00:10:42 +0200

    wrong info due to copying from example and forgetting to add stuff

--- serpend/lookup3/setup.py
+++ serpend/lookup3/setup.py
@@ -1,15 +1,16 @@
 from distutils.core import setup, Extension
 
 module = Extension('lookup3',
-                    define_macros = [('MAJOR_VERSION', '0'), ('MINOR_VERSION', '1')],
+                    define_macros = [('MAJOR_VERSION', '1'), ('MINOR_VERSION', '0')],
                     include_dirs = ['lib/'],
                     sources = ['lib/lookup3.c', 'src/python-lookup3-module.c'])
 
-setup (name = 'py-lookup3',
+setup (name = 'serpend',
        version = '0.1',
-       description = 'Python bindings for fast hashing with lookup3',
-       author = 'Siavash Ghorbani',
-       author_email = 'siavashg@gmail.com',
-       url = 'https://github.com/siavashg/py-lookup3',
-       long_description = 'Python bindings for fast hashing with lookup3',
-       ext_modules = [module])
+       description = 'Python bindings for the systemd hashing function with lookup3',
+       author = 'Arthur de Fluiter',
+       author_email = 'arghhhhthur@gmail.com',
+       url = 'https://github.com/WorkOfArtiz/serpend',
+       long_description = 'systemd journals analysis tool',
+       ext_modules = [module]
+)
